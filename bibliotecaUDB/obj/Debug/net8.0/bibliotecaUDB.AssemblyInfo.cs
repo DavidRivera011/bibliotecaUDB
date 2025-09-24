569e14ab5d50141e6c9032816aec6cbdb5eba518
DavidRivera011 <62643408+DavidRivera011@users.noreply.github.com>
2025-09-24 17:14:36 -0600

    Update cambios de la main a la develop

--- bibliotecaUDB/obj/Debug/net8.0/bibliotecaUDB.AssemblyInfo.cs
+++ bibliotecaUDB/obj/Debug/net8.0/bibliotecaUDB.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("bibliotecaUDB")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+680f6c411f7e788b61434b9a3073957feb46e564")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6621b5b937f0da6398697cf39c43231da58a1195")]
 [assembly: System.Reflection.AssemblyProductAttribute("bibliotecaUDB")]
 [assembly: System.Reflection.AssemblyTitleAttribute("bibliotecaUDB")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
